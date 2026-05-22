761eea9c58f02d1596e3105f4da6c46394920a59
nanthessh <nanthessh777@gmail.com>
2026-05-22 13:01:24 +0530

    fix cors

--- MyApp.API/Program.cs
+++ MyApp.API/Program.cs
@@ -89,8 +89,8 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
 app.UseCors("AllowReact");
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
